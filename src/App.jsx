813d718a801a4fbb1aea9f8e46cdc45535f441de
Guajan <roberto.guajan@Thoughtworks.com>
2022-04-22 13:15:10 -0500

    39. Formulario y Generador de IDS (shortid)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import shortid from "shortid";
 
 function App() {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("Elemento basio");
+      return;
+    }
+    console.log(tarea);
+
+    setTareas([...tareas, { id: shortid.generate(), nombreTarea: tarea }]);
+    setTarea("");
+  };
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD Simple</h1>
@@ -23,6 +38,18 @@ function App() {
         </div>
         <div className="col-4">
           <h4 className="text-center">Form</h4>
+          <form onSubmit={agregarTarea}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Add Task"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button className="btn btn-dark btn-block" type="submit">
+              Add
+            </button>
+          </form>
         </div>
       </div>
     </div>
